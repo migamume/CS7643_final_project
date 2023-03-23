228351b3e979b371834f7e50d697f9c86372b12d
Michelle <michelle.w.lai8@gmail.com>
2023-03-22 20:59:24 -0400

    the model and model training
    Includes rudimentary data prep from parsed csv files assuming we will have 100,000 of such parsed files
    
    random shuffle of the files then split 60:40
    
    Turns each file into tensors, hopefully in the correct shape
    
    Creates flop, turn, and river datasets
    
    Trains the model then does a simple test
    
    There are directives at the bottom for each stage
    
    I added some dependencies in toml, please let me know if this was the right thing to do

---
+++ src/holdup/the_model/autoencoder.py
@@ -0,0 +1,31 @@
+#imports
+import numpy as np
+import torch
+from torch import nn
+import torch.optim as optim
+import random
+
+#build encoder
+class Autoencoder(nn.Module):
+    def __init__(self, num_hidden_nodes):
+        super(Autoencoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(400, num_hidden_nodes),
+            nn.Sigmoid()
+        )
+        self.decoder = nn.Sequential(
+            nn.Linear(num_hidden_nodes, 400),
+            nn.Tanh()
+        )
+        self.softmax = nn.Linear(num_hidden_nodes, 4)
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        softmax_output = self.softmax(encoded)
+        return softmax_output
+
+
+
+
+
